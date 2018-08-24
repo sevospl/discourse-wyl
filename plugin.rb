e3aab0d29c91f25ee280b57924e8ba88bcfba71b
sevospl <sevos@protonmail.ch>
2018-08-24 21:54:29 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,9 +10,6 @@ register_asset "javascripts/discourse/templates/post/poster-avatar.raw.hbs"
 ### Large posts
 register_asset "stylesheets/large-posts.css.scss", :desktop
 
-### Color modifications
-register_asset "stylesheets/colors.css.scss"
-
 ### Spoiler post toolbar button
 #register_asset "javascripts/spoiler-toolbar.js", :composer
 #register_asset "stylesheets/spoiler-toolbar.css.scss"
